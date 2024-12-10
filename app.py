0ff0c4da93fe6bfcaca79ac5af503d4b357784d2
ThePagel <ThePagel@users.noreply.github.com>
2024-12-09 21:45:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-csrf = CSRFProtect(app)
 
 # User model
 class User(db.Model, UserMixin):
